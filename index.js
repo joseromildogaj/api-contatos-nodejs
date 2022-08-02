7e0168f7bc315540bcc3e1724fe54dd00d6450b9
José Romildo <pronuno1@gmail.com>
2022-08-02 01:15:38 -0300

    Construida a lista de objetos com três contatos

--- index.js
+++ index.js
@@ -5,10 +5,33 @@ const porta = process.env.PORT || 3000;
 
 app.use(express.json());
 
+let contatos = [
 
+    {
+        "id": 1,
+        "nome": "Fulano",
+        "fone": "11111111111",
+        "email": "fulano@email.com"
+    },
+    {
+        "id": 2,
+        "nome": "Ciclano",
+        "fone": "22222222222",
+        "email": "ciclano@email.com"
+    },
+    {
+        "id": 3,
+        "nome": "Beltrano",
+        "fone": "33333333333",
+        "email": "beltrano@email.com"
+    }
+]
+
+/*
 app.get('/home', (req, res) => {
     console.log('Página home');
 })
+*/
 
 
 app.listen(porta, () => {
