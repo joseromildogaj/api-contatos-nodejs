194d16e1e8b21b73833d8e077b15ca13d11d1199
José Romildo <pronuno1@gmail.com>
2022-08-02 02:39:43 -0300

    Construção do endpoint que retorna um contato pelo id

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 
 let contatos = [
-    /*
+    
     {
         "id": 1,
         "nome": "Fulano",
@@ -26,12 +26,22 @@ let contatos = [
         "fone": "33333333333",
         "email": "beltrano@email.com"
     }
-    */
+    
 ];
 
-
+// [GET] /contatos - retorna todos os contatos
 app.get('/contatos', (req, res) => {
     res.status(200).json(contatos);
+});
+
+
+// [GET] /contatos - retorna um contato pelo id
+app.get('/contatos/:id', (req, res) => {
+    const indice = contatos.findIndex(item => item.id == req.params.id);
+    if (indice == -1) {
+        return res.status(404).json({});
+    }
+    return res.status(200).json(contatos[indice]);
 });
 
 
